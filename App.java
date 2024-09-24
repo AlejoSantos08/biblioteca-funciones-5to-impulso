f14942e4484e7957d750743d39662609a651eb1d
Alejo Santos <146860991+AlejoSantos08@users.noreply.github.com>
2024-09-24 21:32:26 +0000

    WASA1

--- App.java
+++ App.java
@@ -108,13 +108,24 @@ public class App {
     int arraymaxresult = maxArray (array [5]);
     // Encontrar el máximo en un array
     public static int maxArray(int[] array) {
-        return math.max.maxArray[];
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
     }
-
+int arrayminresult = minArray (array [5]);
     // Encontrar el mínimo en un array
     public static int minArray(int[] array) {
-
-        return 3;
+        int min = array[0];
+        for (int i=1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
     }
 
     // Ordenar un array
